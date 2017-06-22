e9550599491ecb583a77b7447eca6b08f6001b62
MateuszByra <mat.byra@gmail.com>
2017-06-22 18:57:23 +0200

    Part of Houses painting company project.

--- Houses/Program.cs
+++ Houses/Program.cs
@@ -10,6 +10,32 @@ namespace Houses
     {
         static void Main(string[] args)
         {
+            IPainter localPintersCo =
+                new PaintingCompany(
+                    new IPainter[]
+                    {
+                        new Painter("Andy",7),
+                        new Painter("Bill",5)
+                    });
+
+            IPainter bestPaintersCo =
+                new PaintingCompany(
+                    new IPainter[]
+                    {
+                        new Painter("Joe",4),
+                        new Painter("Jill",5),
+                        new Painter("Buster",3),
+                        localPintersCo
+                    });
+
+
+            LandOwner owner = new LandOwner(14, bestPaintersCo);
+
+
+            owner.MaintainHouses();
+
+            Console.WriteLine("Press ENTER to exit ...");
+            Console.ReadLine();
         }
     }
 }
